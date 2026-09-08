4041ddf1962570c18b5950e0bfc0516cebab4c9c
takup-enarize <reo210ta@gmail.com>
2026-09-09 00:15:54 +0900

    Create scan-main.jsx

---
+++ src/scan-main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import ScanApp from './ScanApp.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ScanApp />
+  </React.StrictMode>,
+)
